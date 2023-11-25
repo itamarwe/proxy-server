1109c661240e7e89999d7d5ed2ee247fb7906aba
Itamar Weiss <weiss.itamar@gmail.com>
2023-11-25 09:02:25 +0200

    taking the host port from the environment variable

--- run_docker.sh
+++ run_docker.sh
@@ -4,15 +4,15 @@
 IMAGE_NAME="proxy-server"
 
 # Define the port number
-HOST_PORT=8080
+HOST_PORT=${HOST_PORT:-8080}
 CONTAINER_PORT=8080
+
 
 DESTINATION_URL_VAR=$(printenv DESTINATION_URL)
 
 echo "Running Docker container from image $IMAGE_NAME"
+echo "Docker container is running on port $HOST_PORT"
 
 # Run the container
 docker run -p $HOST_PORT:$CONTAINER_PORT -e DESTINATION_URL=$DESTINATION_URL_VAR $IMAGE_NAME
 
-
-echo "Docker container is running on port $HOST_PORT"
