cdfaa49f7ca908bef8d04902a684239b48d222e7
Itamar Weiss <weiss.itamar@gmail.com>
2023-11-25 14:15:51 +0200

    changing from DESTINATION_URL_VAL to DESTINATION_URL

--- run_docker.sh
+++ run_docker.sh
@@ -8,11 +8,10 @@ HOST_PORT=${HOST_PORT:-8080}
 CONTAINER_PORT=8080
 
 
-DESTINATION_URL_VAR=$(printenv DESTINATION_URL)
+DESTINATION_URL=$(printenv DESTINATION_URL)
 
 echo "Running Docker container from image $IMAGE_NAME"
 echo "Docker container is running on port $HOST_PORT"
 
 # Run the container
-docker run -p $HOST_PORT:$CONTAINER_PORT -e DESTINATION_URL=$DESTINATION_URL_VAR $IMAGE_NAME
-
+docker run -p $HOST_PORT:$CONTAINER_PORT -e DESTINATION_URL=$DESTINATION_URL $IMAGE_NAME
