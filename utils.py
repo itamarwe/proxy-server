85ff082738f9491c5dd2a61dbdc6a6fbe089a9ae
Rotem Shor <>
2023-11-25 14:51:21 +0200

    reconstruct logs

--- utils.py
+++ utils.py
@@ -1,2 +1,2 @@
 def log_request(logger, req, res):
-    logger(f"Request: Header: {req.header}\nMethod:{req.method}\nURL:{req.url}\nPayload: {req.data}\nResponse: {res.status_code}, {res.content}")
+    logger(f"Request: Header: {req.headers}\nMethod:{req.method}\nURL:{req.url}\nPayload: {req.data}\nResponse: {res.status_code}, {res.content}")
