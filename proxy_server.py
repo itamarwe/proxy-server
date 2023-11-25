dfeff2c387899c473e6b94b58fb596d4e6e50420
Rotem Shor <>
2023-11-25 14:54:07 +0200

    reconstruct logs

--- proxy_server.py
+++ proxy_server.py
@@ -17,7 +17,8 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.StreamHandler(sys.stdout)
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler('/var/log/proxy.log')
     ]
 )
 
